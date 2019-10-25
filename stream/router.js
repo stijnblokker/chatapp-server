6b914d7f479b783b8ffb19be03502d02a8cce756
stijnblokker <stijnblokker@gmail.com>
2019-10-25 11:52:09 +0200

    stream made, server side completed

--- stream/router.js
+++ stream/router.js
@@ -1,13 +1,17 @@
 const { Router } = require('express')
 const Chatroom = require('./model')
+const Sse = require('json-sse')
 
 const router = new Router()
+const stream = new Sse()
 
-router.get('/stream', (req, res) => {
+router.get('/stream', async (req, res) => {
     console.log('got a request on /stream')
-    res.status(200)
-    res.send('it works!')
-    
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    console.log('stringified messages in db:', data);
+    stream.updateInit(data)
+    stream.init(req, res)
 })
 
 router.post('/message', async (req, res) => {
@@ -16,6 +20,12 @@ router.post('/message', async (req, res) => {
     const entity = await Chatroom.create({
         message: message
     })
+
+    const messages = await Chatroom.findAll()
+    const data = JSON.stringify(messages)
+    // console.log('stringified messages in db:', data);
+    stream.send(data)
+
     res.status(201)
     res.send('Thanks for your message!') 
 } )
